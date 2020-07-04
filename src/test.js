6a0f832d69c292922d1739bd09ecf112d7b6b984
bens1320 <yu1piao09@163.com>
2020-07-05 02:39:26 +0800

    优化代码为面向对象方式

--- src/test.js
+++ src/test.js
@@ -1,56 +1,64 @@
 import string from "./css.js"
-let n = 1
 
-demo.innerHTML = string.substr(0, n)
-demo2.innerText = string.substr(0, n)
-
-let time = 0
-
-const run = () => {
-    n += 1
-    if(n > string.length){
-        window.clearInterval(id)
-        return
+const player = {
+    n : 1,
+    time : 0,
+    id : undefined,
+    ui: {
+        demo : document.querySelector("#demo"),
+        demo2 : document.querySelector("#demo2")
+    },
+    init : () => {
+        player.ui.demo.innerHTML = string.substr(0, player.n)
+        player.ui.demo2.innerText = string.substr(0, player.n)
+        player.bindEvents()
+        player.play()
+    } ,
+    run : () => {
+        player.n += 1
+        if(player.n > string.length){
+            window.clearInterval(player.id)
+            return
+        }
+        player.ui.demo.innerHTML = string.substr(0, player.n)
+        player.ui.demo2.innerText = string.substr(0, player.n)
+        // 滚动到css最新显示的位置
+        player.ui.demo2.scrollTop = player.ui.demo2.scrollHeight
+    },
+    bindEvents : () => {
+        document.querySelector("#btnPause").onclick = player.pause
+        document.querySelector("#btnPlay").onclick = () => {
+            player.pause()
+            player.play()
+        }
+        document.querySelector("#btnSlow").onclick = player.slow
+        document.querySelector("#btnNormal").onclick = player.normal
+        document.querySelector("#btnFast").onclick = player.fast
+    },
+    play : () => {
+        player.id = setInterval(player.run, player.time)
+    },
+    pause : () => {
+        window.clearInterval(player.id)
+    },
+    slow : () => {
+        player.pause()
+        player.time = 100
+        player.play()
+    },
+    normal : () => {
+        player.pause()
+        player.time = 50
+        player.play()
+    },
+    fast : () => {
+        player.pause()
+        player.time = 0
+        player.play()
     }
-    demo.innerHTML = string.substr(0, n)
-    demo2.innerText = string.substr(0, n)
-    // 滚动到css最新显示的位置
-    demo2.scrollTop = demo2.scrollHeight
+
 }
 
-const play = () => {
-    return setInterval(run, time)
-}
+player.init()
 
-const pause = () => {
-    window.clearInterval(id)
-}
 
-let id = play()
-
-btnPause.onclick = () => {
-    pause()
-}
-
-btnPlay.onclick = () => {
-    pause()
-    id = play()
-}
-
-btnSlow.onclick = () => {
-    pause()
-    time = 200
-    id = play()
-}
-
-btnNormal.onclick = () => {
-    pause()
-    time = 100
-    id = play()
-}
-
-btnFast.onclick = () => {
-    pause()
-    time = 0
-    id = play()
-}
